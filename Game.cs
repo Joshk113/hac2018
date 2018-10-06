2bac649b651f13b423fa250e4d443b22ec90d300
Joshk113 <joshuajkeane@hotmail.com>
2018-10-06 14:07:54 +0100

    Small changes init

--- Game.cs
+++ Game.cs
@@ -1,2 +1,33 @@
-Console.WriteLine("You have just woken...");
-Console.WriteLine("You have no idea where you are, or how you got here...");
+using System;
+using System.Threading;
+
+public static void Main(string[] args)
+{
+    Console.WriteLine("You have just woken...");
+    Console.WriteLine("You have no idea where you are, or how you got here...");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
